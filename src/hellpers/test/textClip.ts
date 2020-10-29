62f63c76b0e28b9c601ca3756097723bdada4cf6
Mladen <blagmash@gmail.com>
2020-10-29 18:31:19 +0100

    textClipt test

---
+++ src/hellpers/test/textClip.ts
@@ -0,0 +1,7 @@
+import textClip from "../textClip";
+
+const testString = 'i am a test string'
+
+test("tex clip to return cliped string with 3 dots atfter cliping point", () => {
+    expect(textClip(testString, 4)).toBe('i am...')
+})
