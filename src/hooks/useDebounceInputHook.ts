1fbb261d854bcaab4b12697c71ceeba2469d3e45
Mladen <blagmash@gmail.com>
2020-10-29 11:54:43 +0100

    all set and go

--- src/hooks/useDebounceInputHook.ts
+++ src/hooks/useDebounceInputHook.ts
@@ -7,15 +7,11 @@ import useDebounce from './useDebounce';
 const useDebounceInputHook = (arg: string, deley) => {
     const [value, setValue] = useState()
 
-    const onChange = (e) => {
-        const [debounced] = useDebounce(e.target.value, deley)
-        setValue(debounced)
-
-    }
 
 
 
-    return [value, onChange]
+
+
 }
 
 export default useDebounceInputHook
