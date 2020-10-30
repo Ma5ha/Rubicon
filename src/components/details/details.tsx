87279233df8da577d63326c85c7d210e996d862d
Mladen <blagmash@gmail.com>
2020-10-30 10:07:03 +0100

    details typed

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 
+interface props {
+  children: Element[] | Element | string;
+}
+
 export const Title = ({ children }) => {
   return <h1 className="title">{children}</h1>;
 };
 
-export const Overview = ({ children }) => {
+export const Overview = ({ children }: props) => {
   return <p className="overview">{children}</p>;
 };
 
-export const Creator = ({ children }) => {
-  return (
-    <div>
-      <h5>{[children[0]?.name]}</h5> <h6>Creator</h6>
-    </div>
-  );
-};
+interface trialer {
+  video: string;
+}
 
-export const Trailer = ({ video }) => {
+export const Trailer = ({ video }: trialer) => {
   const src = `https://www.youtube.com/embed/${video} `;
   return (
     <iframe width="100%" height="315" {...{ src }} title="no title"></iframe>
